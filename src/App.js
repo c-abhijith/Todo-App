316d35184f68260fa4746c70e6cd4565c1061635
Abhijith <abhijithabhipgmr145@gmail.com>
2022-06-24 20:56:08 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,90 @@
+import logo from './logo.svg';
+import './App.css';
+import React,{useState} from 'react';
+import Todo from './Components/Todo';
+
+function App() {
+  const [toDos,setToDos]=useState([])
+  const [toDo,setToDo]=useState("")
+  const current = new Date();
+  const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
+
+
+  const func = (id) => {
+    alert("Do you want delete")
+    setToDos(toDos.filter(obj3=>{
+           if(obj3.id === id){
+             console.log(obj3.text)
+
+             return false
+           }
+           return true
+    }))
+  }
+  
+
+  return (
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's {date} 🌝 ☕ </h2>
+        
+      </div>
+      <div className="input">
+        <input value={toDo} onChange={(event)=>{setToDo(event.target.value)}} type="text" placeholder="🖊️ Add item..." />
+        <i 
+        onClick={()=>{
+          if(toDo === "")
+          {
+            alert("The field was Empty")
+          }
+          else{
+
+            setToDos([...toDos,{id:Date.now() ,text:toDo,status:false}]);
+            setToDo('')
+          }
+      }} className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+        {
+          toDos.map((obj)=>{
+
+          return( 
+            
+            <div className="todo">
+          <div className="left">
+            <input onChange={(e)=>{console.log(e.target.checked)
+            console.log(obj)
+            setToDos(toDos.filter(obj2=>{
+              if(obj2.id === obj.id)
+              {
+                obj2.status=e.target.checked
+              }
+              return obj2 
+            }))
+          }} 
+            value={obj.status} type="checkbox" name="" id="" />
+            <p>{obj.text}</p>
+          </div>
+          <div className="right">
+            <i className="fas fa-times" onClick={() => func(obj.id)}></i>
+          </div>
+        </div>
+              )
+        })}
+
+        {toDos.map((obj)=>{
+          if(obj.status){
+            return (<h1>{obj.text}</h1>)
+          }
+          return null
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
